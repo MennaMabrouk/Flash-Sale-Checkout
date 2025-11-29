8139abc86ceb6776805ef0efc955824383b9806c
Menna Mabrouk Aly <mmaly569@gmail.com>
2025-11-29 03:19:12 +0200

    Add Product model,migration,seeding

--- FlashSaleCheckout/routes/api.php
+++ FlashSaleCheckout/routes/api.php
@@ -2,12 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::get('welcome',function()
-{
-return 'Welcome to api';
-});
+Route::get('/products/{id}', [ProductController::class, 'show']);
